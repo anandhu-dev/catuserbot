6cbe99f34aac4f36c03d69ebc32af014569c7e86
anandhu-dev <54721685+anandhu-dev@users.noreply.github.com>
2020-05-21 07:11:30 +0530

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -27,24 +27,26 @@ def bruh(name):
 async def _(event):
     if event.fwd_from:
         return
-    DELAY_BETWEEN_EDITS = 0.3
+    DELAY_BETWEEN_EDITS = 0.8
     PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.edit("ok finding the song")    
+    await event.edit("`---- just a sec --- `"),
+    await event.edit("`wi8..! i am finding your song.. 😴`"),
     bruh(str(cmd))
     l = glob.glob("*.mp3")
     loa = l[0]
-    await event.edit("sending song")
+    await event.edit("`yeah..! i find something wi8..`")
     await borg.send_file(
                 event.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
-                caption=cmd,
+                caption="`Uploaded by` : [Kannan](@kannappan04) 👻 \n`Channel` : [പാട്ടുപെട്ടി](t.me/puthiyapaattukal) ✅",
                 reply_to=reply_to_id
             )
+    await event.delete()
     os.system("rm -rf *.mp3")
     subprocess.check_output("rm -rf *.mp3",shell=True)
